1088d99e85514ad82747125b7b561a6917b6fd88
FX-Wood <fxwood2@gmail.com>
2019-02-21 17:23:14 -0800

    working root route

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const db = require('./models')
 require('dotenv').config()
 
 const app = express();
 
 
 app.use(express.static('static'))
+app.use(express.urlencoded({extended:true}))
+
+app.route('/')
+    .get(function(req,res) {
+        db.show.findAll()
+        .then(function(data){
+            res.json(data)
+        })
+    })
+    .post(function(req, res) {
+        db.show.create({
+            title: req.body.title,
+            description: req.body.description,
+            rating: req.body.rating
+        })
+        .then(function(data) {
+            res.redirect('/')
+        })
+    })
+
 
 app.listen(process.env.PORT
     , function() {
         console.log('crud-api is running');
-        console.log('listening on port: ' + process.env.port);
+        console.log('listening on port: ' + process.env.PORT);
     })
